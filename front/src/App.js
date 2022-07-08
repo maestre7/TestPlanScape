d0c0486a24fbf5138b404429c405bdbecfe81890
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2022-07-09 01:12:13 +0200

    register done

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,15 @@
 
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Login />} />
-                {/* <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/register" element={<Register />} />
+                {/* <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/post" element={<Post />} /> */}
             </Routes>
         </div>
